44cff682bc0379acf1c54e7169d67d29bd647adf
Dedi Setiawan <dedi.setiawan@primecash.co.id>
2024-02-05 20:40:25 +0700

    add product api

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function login(Request $request) {
+        $loginData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = \App\Models\User::where('email', $request->email)->first();
+
+        if(!$user){
+            return response([
+                'message' => ['Akun belum terdaftar']
+            ], 400);
+        }
+
+        if(!Hash::check($request->password, $user->password)){
+            return response([
+                'message' => ['email/password salah']
+            ], 400);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], 200);
+    }
+}
